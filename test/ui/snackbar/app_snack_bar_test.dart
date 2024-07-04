7693cb94187f4f1f2a2f213397b709d99661e76e
SaurabhS120 <saurabh.sonar120@gmail.com>
2024-07-04 10:09:56 +0530

    Snack bar test case grouping

--- test/ui/snackbar/app_snack_bar_test.dart
+++ test/ui/snackbar/app_snack_bar_test.dart
@@ -5,113 +5,117 @@ import 'package:green_bank/ui/snackbar/app_snack_bar_impl.dart';
 
 void main() {
   group("Default Snack bar tests", (){
-    test("Success snack bar unit test", (){
-      const SnackBarType snackBarType = SnackBarType.success;
-      SnackBarFactory snackBarFactory = const SnackBarFactoryImpl();
-      SnackBarShower snackBarShower = snackBarFactory.createSnackBar(snackBarType);
-      expect(snackBarShower, isA<SuccessSnackBarShower>(), reason: "Should show success snack bar for success type");
+    group("Unit test", (){
+      test("Success snack bar unit test", (){
+        const SnackBarType snackBarType = SnackBarType.success;
+        SnackBarFactory snackBarFactory = const SnackBarFactoryImpl();
+        SnackBarShower snackBarShower = snackBarFactory.createSnackBar(snackBarType);
+        expect(snackBarShower, isA<SuccessSnackBarShower>(), reason: "Should show success snack bar for success type");
+      });
+      test("Warning snack bar unit test", (){
+        const SnackBarType snackBarType = SnackBarType.warning;
+        SnackBarFactory snackBarFactory = const SnackBarFactoryImpl();
+        SnackBarShower snackBarShower = snackBarFactory.createSnackBar(snackBarType);
+        expect(snackBarShower, isA<WarningSnackBarShower>(), reason: "Should show warning snack bar for warning type");
+      });
+      test("Error snack bar unit test", (){
+        const SnackBarType snackBarType = SnackBarType.error;
+        SnackBarFactory snackBarFactory = const SnackBarFactoryImpl();
+        SnackBarShower snackBarShower = snackBarFactory.createSnackBar(snackBarType);
+        expect(snackBarShower, isA<ErrorSnackBarShower>(), reason: "Should show error snack bar for error type");
+      });
     });
-    test("Warning snack bar unit test", (){
-      const SnackBarType snackBarType = SnackBarType.warning;
-      SnackBarFactory snackBarFactory = const SnackBarFactoryImpl();
-      SnackBarShower snackBarShower = snackBarFactory.createSnackBar(snackBarType);
-      expect(snackBarShower, isA<WarningSnackBarShower>(), reason: "Should show warning snack bar for warning type");
-    });
-    test("Error snack bar unit test", (){
-      const SnackBarType snackBarType = SnackBarType.error;
-      SnackBarFactory snackBarFactory = const SnackBarFactoryImpl();
-      SnackBarShower snackBarShower = snackBarFactory.createSnackBar(snackBarType);
-      expect(snackBarShower, isA<ErrorSnackBarShower>(), reason: "Should show error snack bar for error type");
-    });
-    testWidgets("Success Snack Bar Test", (WidgetTester tester) async {
-      const Key targetKey = Key('showSnackBarButton');
-      const String title = "Success";
-      const SnackBarType snackBarType = SnackBarType.success;
-      await tester.pumpWidget(
-        MaterialApp(
-          home: Scaffold(
-            body: Center(
-              child: Builder(builder: (context) {
-                return ElevatedButton(
-                  key: targetKey,
-                  onPressed: () {
-                    AppSnackBar.showSnackBar(context,
-                        message: title, type: snackBarType);
-                  },
-                  child: const Text('Show SnackBar'),
-                );
-              }),
+    group("Widget test", (){
+      testWidgets("Success Snack Bar Test", (WidgetTester tester) async {
+        const Key targetKey = Key('showSnackBarButton');
+        const String title = "Success";
+        const SnackBarType snackBarType = SnackBarType.success;
+        await tester.pumpWidget(
+          MaterialApp(
+            home: Scaffold(
+              body: Center(
+                child: Builder(builder: (context) {
+                  return ElevatedButton(
+                    key: targetKey,
+                    onPressed: () {
+                      AppSnackBar.showSnackBar(context,
+                          message: title, type: snackBarType);
+                    },
+                    child: const Text('Show SnackBar'),
+                  );
+                }),
+              ),
             ),
           ),
-        ),
-      );
-      await tester.tap(find.byKey(targetKey));
-      await tester.pumpAndSettle();
-      expect(find.byType(SnackBar), findsOneWidget);
-      Finder snackBarTitleFinder = find.byKey(const Key('snackBarTitle'));
-      expect(snackBarTitleFinder, findsOneWidget);
-      Text snackBarTitleText = tester.widget(snackBarTitleFinder);
-      expect(snackBarTitleText.data, title);
-    });
-    testWidgets("Warning Snack Bar Test", (WidgetTester tester) async {
-      const Key targetKey = Key('showSnackBarButton');
-      const String title = "Warning";
-      const SnackBarType snackBarType = SnackBarType.warning;
-      await tester.pumpWidget(
-        MaterialApp(
-          home: Scaffold(
-            body: Center(
-              child: Builder(builder: (context) {
-                return ElevatedButton(
-                  key: targetKey,
-                  onPressed: () {
-                    AppSnackBar.showSnackBar(context,
-                        message: title, type: snackBarType);
-                  },
-                  child: const Text('Show SnackBar'),
-                );
-              }),
+        );
+        await tester.tap(find.byKey(targetKey));
+        await tester.pumpAndSettle();
+        expect(find.byType(SnackBar), findsOneWidget);
+        Finder snackBarTitleFinder = find.byKey(const Key('snackBarTitle'));
+        expect(snackBarTitleFinder, findsOneWidget);
+        Text snackBarTitleText = tester.widget(snackBarTitleFinder);
+        expect(snackBarTitleText.data, title);
+      });
+      testWidgets("Warning Snack Bar Test", (WidgetTester tester) async {
+        const Key targetKey = Key('showSnackBarButton');
+        const String title = "Warning";
+        const SnackBarType snackBarType = SnackBarType.warning;
+        await tester.pumpWidget(
+          MaterialApp(
+            home: Scaffold(
+              body: Center(
+                child: Builder(builder: (context) {
+                  return ElevatedButton(
+                    key: targetKey,
+                    onPressed: () {
+                      AppSnackBar.showSnackBar(context,
+                          message: title, type: snackBarType);
+                    },
+                    child: const Text('Show SnackBar'),
+                  );
+                }),
+              ),
             ),
           ),
-        ),
-      );
-      await tester.tap(find.byKey(targetKey));
-      await tester.pumpAndSettle();
-      expect(find.byType(SnackBar), findsOneWidget);
-      Finder snackBarTitleFinder = find.byKey(const Key('snackBarTitle'));
-      expect(snackBarTitleFinder, findsOneWidget);
-      Text snackBarTitleText = tester.widget(snackBarTitleFinder);
-      expect(snackBarTitleText.data, title);
-    });
-    testWidgets("Error Snack Bar Test", (WidgetTester tester) async {
-      const Key targetKey = Key('showSnackBarButton');
-      const String title = "Error";
-      const SnackBarType snackBarType = SnackBarType.error;
-      await tester.pumpWidget(
-        MaterialApp(
-          home: Scaffold(
-            body: Center(
-              child: Builder(builder: (context) {
-                return ElevatedButton(
-                  key: targetKey,
-                  onPressed: () {
-                    AppSnackBar.showSnackBar(context,
-                        message: title, type: snackBarType);
-                  },
-                  child: const Text('Show SnackBar'),
-                );
-              }),
+        );
+        await tester.tap(find.byKey(targetKey));
+        await tester.pumpAndSettle();
+        expect(find.byType(SnackBar), findsOneWidget);
+        Finder snackBarTitleFinder = find.byKey(const Key('snackBarTitle'));
+        expect(snackBarTitleFinder, findsOneWidget);
+        Text snackBarTitleText = tester.widget(snackBarTitleFinder);
+        expect(snackBarTitleText.data, title);
+      });
+      testWidgets("Error Snack Bar Test", (WidgetTester tester) async {
+        const Key targetKey = Key('showSnackBarButton');
+        const String title = "Error";
+        const SnackBarType snackBarType = SnackBarType.error;
+        await tester.pumpWidget(
+          MaterialApp(
+            home: Scaffold(
+              body: Center(
+                child: Builder(builder: (context) {
+                  return ElevatedButton(
+                    key: targetKey,
+                    onPressed: () {
+                      AppSnackBar.showSnackBar(context,
+                          message: title, type: snackBarType);
+                    },
+                    child: const Text('Show SnackBar'),
+                  );
+                }),
+              ),
             ),
           ),
-        ),
-      );
-      await tester.tap(find.byKey(targetKey));
-      await tester.pumpAndSettle();
-      expect(find.byType(SnackBar), findsOneWidget);
-      Finder snackBarTitleFinder = find.byKey(const Key('snackBarTitle'));
-      expect(snackBarTitleFinder, findsOneWidget);
-      Text snackBarTitleText = tester.widget(snackBarTitleFinder);
-      expect(snackBarTitleText.data, title);
+        );
+        await tester.tap(find.byKey(targetKey));
+        await tester.pumpAndSettle();
+        expect(find.byType(SnackBar), findsOneWidget);
+        Finder snackBarTitleFinder = find.byKey(const Key('snackBarTitle'));
+        expect(snackBarTitleFinder, findsOneWidget);
+        Text snackBarTitleText = tester.widget(snackBarTitleFinder);
+        expect(snackBarTitleText.data, title);
+      });
     });
   });
 }
