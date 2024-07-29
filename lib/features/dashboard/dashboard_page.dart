21c29c4f0edaed370fe00b9b533ff540f13e7d22
SaurabhS120 <saurabh.sonar120@gmail.com>
2024-07-29 17:33:03 +0530

    Dashboard page loader with test

--- lib/features/dashboard/dashboard_page.dart
+++ lib/features/dashboard/dashboard_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:green_bank/features/dashboard/user_details_bloc.dart';
+import 'package:green_bank/ui/app_loader.dart';
 
 class DashboardPage extends StatefulWidget{
   const DashboardPage({super.key});
@@ -13,18 +14,31 @@ class DashboardPage extends StatefulWidget{
 class _DashboardPageState extends State<DashboardPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar:
-      AppBar(
-        backgroundColor: Colors.green,
-        title: BlocBuilder<UserDetailsBloc,UserDetailsBlocState>(
-            builder: (context,state){
-              String name = "User";
-              if(state is UserDetailsSuccessBlocState){
-                name = state.name;
+    return BlocListener<UserDetailsBloc,UserDetailsBlocState>(
+      listener: (BuildContext context, state) {
+        if(state is UserDetailsLoadingBlocState){
+          showDialog(
+              context: context,
+              builder: (context)=>const AppLoader(),
+              barrierDismissible: false,
+          );
+        }else if(state is UserDetailsSuccessBlocState){
+          Navigator.of(context).pop();
+        }
+      },
+      child: Scaffold(
+        appBar:
+        AppBar(
+          backgroundColor: Colors.green,
+          title: BlocBuilder<UserDetailsBloc,UserDetailsBlocState>(
+              builder: (context,state){
+                String name = "User";
+                if(state is UserDetailsSuccessBlocState){
+                  name = state.name;
+                }
+                return Text(key: const Key("user-greet"),"Hello, $name");
               }
-              return Text(key: const Key("user-greet"),"Hello, $name");
-            }
+          ),
         ),
       ),
     );
